2164fb696d8fb25454a04b8550fc3333725a0891
Helen Li <hca1314@gmail.com>
2019-04-16 14:30:08 -0500

    changed app.js from functional to class component, started all-sunglasses component

--- client/app.js
+++ client/app.js
@@ -1,15 +1,36 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { fetchSunglasses } from '../client/store/sunglasses'
 
 import {Navbar} from './components'
 import Routes from './routes'
+import { Switch, Route} from 'react-router-dom'
+import AllSunglasses from '../client/components/all-sunglasses'
 
-const App = () => {
-  return (
-    <div>
+class DisconnectedApp extends React.Component {
+  componentDidMount() {
+    this.props.fetchInitialSunglasses()
+  }
+
+  render() {
+    return (
+      <div>
       <Navbar />
       <Routes />
+      <Switch>
+        <Route exact path="/sunglasses" component={AllSunglasses}/>
+      </Switch>
     </div>
-  )
+    )
+  }
 }
+
+const mapDispatch = dispatch => {
+  return ({
+    fetchInitialSunglasses: () => dispatch(fetchSunglasses())
+  })
+}
+
+const App = connect(null, mapDispatch)(DisconnectedApp)
 
 export default App
---
+++ client/components/all-sunglasses.js
@@ -0,0 +1,13 @@
+import react from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+class DisconnectedAllSunglasses extends React.Component {
+    render() {
+        return (
+
+        )
+    }
+}
+
+export default AllSunglasses
