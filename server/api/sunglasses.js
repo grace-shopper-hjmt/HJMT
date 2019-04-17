fbafe6dcb9a92afa0ed1a1707ffdc73679e78cf2
mchang22 <43551957+mchang22@users.noreply.github.com>
2019-04-17 10:53:22 -0500

    finish delete route and thunk and redux store (#12)
    * finish delete route and thunk and redux store
    
    * delete sunglasses part added graceShopper test

--- server/api/sunglasses.js
+++ server/api/sunglasses.js
@@ -9,5 +9,13 @@ router.get('/', async (req, res, next) => {
        next(error)
    }
 })
-
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const sunglasses = await Sunglasses.findById(req.params.id)
+    await sunglasses.destroy()
+    res.sendStatus(204)
+  } catch (error) {
+    next(error)
+  }
+})
 module.exports = router
