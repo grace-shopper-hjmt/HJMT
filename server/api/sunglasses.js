cb7d229a04093815f827dd6c176b1922d1b7a7a8
Helen Li <hca1314@gmail.com>
2019-04-17 11:11:37 -0500

    wrote working api route for getting single pair of sunglasses by pk (id)

--- server/api/sunglasses.js
+++ server/api/sunglasses.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { Sunglasses } = require('../db/models')
+const { Sunglasses, Reviews } = require('../db/models')
 
 router.get('/', async (req, res, next) => {
    try {
@@ -8,6 +8,26 @@ router.get('/', async (req, res, next) => {
    } catch (error) {
        next(error)
    }
+})
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const sunglasses = await Sunglasses.findByPk(req.params.id, {
+            include: [{model: Reviews}]
+        })
+
+        if (!sunglasses) {
+            const error = new Error('This pair of sunglasses does not exist!')
+            error.status = 404
+            next(error)
+        }
+        else {
+            res.json(sunglasses) 
+        }
+    }
+    catch (error) {
+        next(error)
+    }
 })
 
 module.exports = router
