438dda26d77f0ae7fb13dc85c839a6ac2d40d9e7
Michael Chang <mchang22@buffalo.edu>
2019-04-17 16:56:36 -0500

    done name and price

--- client/components/NewSunglasses.js
+++ client/components/NewSunglasses.js
@@ -13,26 +13,65 @@ export class NewSunglasses extends Component{
       imageUrl: '',
       description: '',
       inventory: '',
-      brand: '',
-      color: '',
-      shape: ''
+      errorMessage: '',
+      warning:'Field is required'
     }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
-  handleChange(event) { }
-  handleSubmit(event) { }
+  handleChange(event) {
+    this.setState({
+      [event.target.name]:event.target.value
+    })
+   }
+  handleSubmit(event) {
+    event.preventDefault()
+    try {
+      this.props.thunkAddSunglasses({...this.state})
+    } catch (error) {
+      this.setState({
+        errorMessage:"There was a problem creating sunglasses: " + error.message
+      })
+    }
+   }
 
   render() {
-    const { name, price, imageUrl, description, inventory, brand, color, shape } = this.state
+    const { name, price, imageUrl, description, inventory, errorMessage, warning} = this.state
     return (
       <div>
         <main>
           <h1>Add new sunglasses here!</h1>
-          <form>
+          <form onSubmit={this.handleSubmit}>
+
             <label>
-
-
-
+            Name:
+                {!name && warning && <span className='warning'>{warning}</span>}
+              <input
+                onChange={this.handleChange}
+                name='name'
+                type='text'
+                value={name}
+              />
             </label>
+
+            <label>
+            Price:
+                {!price && warning && <span className='warning'>{warning}</span>}
+              <input
+                onChange={this.handleChange}
+                price='price'
+                type='number'
+                step='.01'
+                value={price}
+              />
+            </label>
+
+
+
+
+
+
+<button type='submit'>Submit</button>
           </form>
           <h4>
             <Link to='/sunglasses'>Back</Link>
