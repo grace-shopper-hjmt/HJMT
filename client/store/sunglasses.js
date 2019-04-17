2f4b925394a5185cc049675e28df9e21d5e6eced
Janelle Henney <janellehenney@gmail.com>
2019-04-17 10:39:42 -0500

    api put route for sunglasses, redux store for update sunglasses

--- client/store/sunglasses.js
+++ client/store/sunglasses.js
@@ -2,33 +2,77 @@ import axios from 'axios'
 import history from '../history'
 
 const initialState = {
-    allSunglasses: [],
-    selectedSunglasses: {}
+  allSunglasses: [],
+  selectedSunglasses: {}
 }
 
-const GET_ALL_SUNGLASSES ='GET_ALL_SUNGLASSES'
+//ACTION TYPES
+const GET_ALL_SUNGLASSES = 'GET_ALL_SUNGLASSES'
+const EDIT_SUNGLASSES = 'EDIT_SUNGLASSES'
 
-export const getAllSunglasses = (sunglasses) => ({type: GET_ALL_SUNGLASSES, sunglasses})
+//ACTION CREATORS
+export const getAllSunglasses = sunglasses => ({
+  type: GET_ALL_SUNGLASSES,
+  sunglasses
+})
+export const editSunglasses = (id, sunglasses) => ({
+  type: EDIT_SUNGLASSES,
+  id,
+  sunglasses
+})
 
-const handlers = {
-    [GET_ALL_SUNGLASSES]: (state, action) => ({...state, allSunglasses: action.sunglasses})
-}
-
+//THUNKS
 export const fetchSunglasses = () => {
- return async dispatch => {
-     try { 
-         const { data } = await axios.get('/api/sunglasses')
-         dispatch(getAllSunglasses(data))
-     } catch (error) {
-        console.log('ERROR FETCHING SUNGLASSES', error)
-     }
- }
+  return async dispatch => {
+    try {
+      const {data} = await axios.get('/api/sunglasses')
+      dispatch(getAllSunglasses(data))
+    } catch (error) {
+      console.log('ERROR FETCHING SUNGLASSES', error)
+    }
+  }
+}
+
+export const updateSunglasses = (id, sunglasses) => {
+  return async dispatch => {
+    try {
+      await axios.put(`/api/sunglasses/${id}`, sunglasses)
+      dispatch(editSunglasses(id, sunglasses))
+    } catch (err) {
+      console.log('ERROR updating those sunglasses', err)
+    }
+  }
+}
+
+//HANDLERS FOR SUNGLASSES REDUCER
+const handlers = {
+  [GET_ALL_SUNGLASSES]: (state, action) => ({
+    ...state,
+    allSunglasses: action.sunglasses
+  }),
+  [EDIT_SUNGLASSES]: (state, action) => {
+    if (state.selectedSunglasses.id === action.id) {
+      return {
+        selectedSunglasses: action.sunglasses,
+        allSunglasses: state.allSunglasses
+          .filter(sunglasses => sunglasses.id !== action.id)
+          .push(action.sunglasses)
+      }
+    } else {
+      return {
+        ...state,
+        allSunglasses: state.allSunglasses
+          .filter(sunglasses => sunglasses.id !== action.id)
+          .push(action.sunglasses)
+      }
+    }
+  }
 }
 
 export const sunglassesReducer = (state = initialState, action) => {
-    if (handlers.hasOwnProperty(action.type)) {
-        return handlers[action.type](state, action)
-    }
+  if (handlers.hasOwnProperty(action.type)) {
+    return handlers[action.type](state, action)
+  }
 
-    return state
+  return state
 }
--- server/api/sunglasses.js
+++ server/api/sunglasses.js
@@ -1,13 +1,48 @@
 const router = require('express').Router()
-const { Sunglasses } = require('../db/models')
+const {Sunglasses} = require('../db/models')
 
 router.get('/', async (req, res, next) => {
-   try {
-       const sunglasses = await Sunglasses.findAll()
-       res.json(sunglasses)
-   } catch (error) {
-       next(error)
-   }
+  try {
+    const sunglasses = await Sunglasses.findAll()
+    res.json(sunglasses)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const sunglasses = await Sunglasses.findByPk(req.params.id)
+    if (!sunglasses) {
+      const err = new Error('sunglasses not found!')
+      err.status = 404
+      return next(err)
+    }
+    const {
+      id,
+      name,
+      price,
+      imageUrl,
+      description,
+      inventory,
+      brand,
+      color,
+      shape
+    } = await Sunglasses.update({...req.body})
+    res.json({
+      id,
+      name,
+      price,
+      imageUrl,
+      description,
+      inventory,
+      brand,
+      color,
+      shape
+    })
+  } catch (err) {
+    next(err)
+  }
 })
 
 module.exports = router
