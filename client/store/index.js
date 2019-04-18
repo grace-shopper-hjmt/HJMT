8a87cd8e70720849a26f7a15a189b4b6dfcd2424
Janelle Henney <janellehenney@gmail.com>
2019-04-18 16:40:14 -0500

    fixed the dumb thing I was working on

--- client/store/index.js
+++ client/store/index.js
@@ -2,12 +2,12 @@ import {createStore, combineReducers, applyMiddleware} from 'redux'
 import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
-import user from './user'
+import userReducer from './user'
 import {sunglassesReducer} from '../store/sunglasses'
 // import {adminReducer} from './admin'
 
 const reducer = combineReducers({
-  user,
+  user: userReducer,
   sunglasses: sunglassesReducer
   // admin: adminReducer
 })
--- client/store/user.js
+++ client/store/user.js
@@ -47,15 +47,26 @@ export const logout = () => async dispatch => {
   }
 }
 
-// HANDLERS FOR USERS REDUCER
-const handlers = {
-  [LOGIN_USER]: action => ({defaultUser: action.user}),
-  [LOGOUT_USER]: () => ({defaultUser: {}})
-}
+// // HANDLERS FOR USERS REDUCER
+// const handlers = {
+//   [LOGIN_USER]: (state, action) => ({defaultUser: action.user}),
+//   [LOGOUT_USER]: (state, action) => ({defaultUser})
+// }
+
+// export default function(state = defaultUser, action) {
+//   if (handlers.hasOwnProperty(action.type)) {
+//     return handlers[action.type](state, action)
+//   }
+//   return state
+// }
 
 export default function(state = defaultUser, action) {
-  if (handlers.hasOwnProperty(action.type)) {
-    return handlers[action.type](state, action)
+  switch (action.type) {
+    case LOGIN_USER:
+      return action.user
+    case LOGOUT_USER:
+      return defaultUser
+    default:
+      return state
   }
-  return state
 }
