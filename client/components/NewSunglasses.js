0a21219f667c90a70a1f7cae1fc91d94e35961c7
mchang22 <43551957+mchang22@users.noreply.github.com>
2019-04-18 10:42:02 -0500

    New sunglasses page (#26)
    * done half of it but need revise
    
    * done name and price
    
    * finish the new sunglasses form
    
    * fixed some required fields and inventory model change
    
    * fixed type field on inventory and imgUrl

---
+++ client/components/NewSunglasses.js
@@ -0,0 +1,120 @@
+/* eslint-disable complexity */
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { thunkAddSunglasses } from '../store/sunglasses'
+import { Link } from "react-router-dom";
+
+
+export class NewSunglasses extends Component{
+  constructor() {
+    super()
+    this.state = {
+      name: '',
+      price: '',
+      imageUrl: '',
+      description: '',
+      inventory: '',
+      warning:'Field is required'
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]:event.target.value
+    })
+   }
+  handleSubmit(event) {
+    event.preventDefault()
+    try {
+      this.props.thunkAddSunglasses({...this.state})
+    } catch (error) {
+      console.error('Cannot submit the form')
+    }
+   }
+
+  render() {
+    const { name, price, imageUrl, description, inventory, warning} = this.state
+    return (
+      <div>
+        <main>
+          <h1>Add new sunglasses here!</h1>
+          <form onSubmit={this.handleSubmit}>
+
+            <label>
+            Name:
+                {!name && warning && <span className='warning'>{warning}</span>}
+              <input
+                onChange={this.handleChange}
+                name='name'
+                type='text'
+                value={name}
+              />
+            </label>
+
+            <label>
+            Price:
+                {!price && warning && <span className='warning'>{warning}</span>}
+              <input
+                onChange={this.handleChange}
+                name='price'
+                type='number'
+                step='.01'
+                value={price}
+              />
+            </label>
+
+            <label>
+            imageUrl:
+              <input
+                onChange={this.handleChange}
+                name='imageUrl'
+                type='text'
+                value={imageUrl}
+              />
+            </label>
+
+            <label>
+            description:
+              <input
+                onChange={this.handleChange}
+                name='description'
+                type='text'
+                value={description}
+              />
+            </label>
+
+            <label>
+            inventory:
+                {!inventory && warning && <span className='warning'>{warning}</span>}
+              <input
+                onChange={this.handleChange}
+                name='inventory'
+                type='number'
+                value={inventory}
+              />
+            </label>
+
+
+<button type='submit'>Submit</button>
+          </form>
+          <h4>
+            <Link to='/sunglasses'>Back</Link>
+          </h4>
+          </main>
+        </div>
+    )
+}
+}
+
+
+const mapDispatch = (dispatch, ownProps) => {
+  return {
+    thunkAddSunglasses: newProps => {
+      dispatch(thunkAddSunglasses(newProps, ownProps))
+   }
+  }
+}
+
+export default connect(null, mapDispatch)(NewSunglasses)
