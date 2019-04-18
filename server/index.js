b42d898cdcf55e44d8e67c04b9d61905f8306914
Janelle Henney <janellehenney@gmail.com>
2019-04-18 14:35:57 -0500

    did the google oauth

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const session = require('express-session')
 const passport = require('passport')
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
 const db = require('./db')
+const {User} = require('./db/models')
 const sessionStore = new SequelizeStore({db})
 const PORT = process.env.PORT || 8080
 const app = express()
@@ -33,7 +34,8 @@ passport.serializeUser((user, done) => done(null, user.id))
 
 passport.deserializeUser(async (id, done) => {
   try {
-    const user = await db.models.user.findById(id)
+    console.log('MODELS', db.models)
+    const user = await User.findByPk(id)
     done(null, user)
   } catch (err) {
     done(err)
