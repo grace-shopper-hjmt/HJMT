46cffbc4f0b1f41255626c4e3f96aadb07b086e7
Tim Ryding <tryding2@gmail.com>
2019-04-19 16:14:56 -0500

    MERGE CONFLICCCCCCCTS

--- server/api/index.js
+++ server/api/index.js
@@ -3,11 +3,8 @@ module.exports = router
 
 router.use('/users', require('./users'))
 router.use('/sunglasses', require('./sunglasses'))
-<<<<<<< HEAD
 router.use('/categories', require('./categories'))
-=======
 router.use('/cart', require('./cart') )
->>>>>>> master
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
