2e6f0ca191ccc5ea6f34d2b702b27fd8c427e287
Helen Li <hca1314@gmail.com>
2019-04-19 21:14:03 -0500

    modified get all cart items route to eager load product details so they can  be rendered on client side upon viewing cart

--- server/api/cart.js
+++ server/api/cart.js
@@ -1,16 +1,17 @@
 //how to handle carts for logged in users and users that aren't logged in?
 const router = require('express').Router()
 const Sequelize = require('sequelize')
-const { CartItems } = require('../db/models')
+const { CartItems, Sunglasses} = require('../db/models')
 
 router.get('/', async (req, res, next) => {
     try {
         const cartItems = await CartItems.findAll({
             where: {
                 userId: req.user.id
-            }
+            }, 
+            
+            include: {model: Sunglasses}
         })
-
         res.json(cartItems)
     } catch (error) {
         next(error)
