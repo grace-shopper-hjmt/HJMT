5c1fbe22ae647f231baaf3c09c14fc1479a4f260
lp1295 <46366814+lp1295@users.noreply.github.com>
2019-04-22 16:47:40 -0500

    Update cart quantities (#51)
    * added removeItem method to cart component and connected to cart-items component
    
    * wrote out removeItem method in cart component
    
    * wrote axios delete route to remove single product type from cart, added to removeItem method in cart component
    
    * added handleChange method to cart component
    
    * added checkout component, moved placeOrder method from cart to checkout
    
    * added min (1) and max (number in inventory) values for cart items
    
    * added to order post route so sunglasses inventory updates based on orders created
    
    * added check for whether there are items in cart in checkout component

--- client/components/cart.js
+++ client/components/cart.js
@@ -5,66 +5,80 @@ import {CartItem} from './cart-item'
 import Button from '@material-ui/core/Button'
 
 class DisconnectedCart extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      cartItems: []
+    constructor() {
+        super()
+        this.state = {
+            cartItems: []
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.removeItem = this.removeItem.bind(this)
+        this.updateCart = this.updateCart.bind(this)
     }
 
-    this.placeOrder = this.placeOrder.bind(this)
-  }
+    handleChange(event, index) {
+        const { cartItems } = this.state
+        cartItems[index].quantity = event.target.value
+        this.setState({cartItems: cartItems})
+    }
 
-  placeOrder() {
-    const orders = this.state.cartItems.map(item => {
-      return {
-        name: item.sunglass.name,
-        description: item.sunglass.description,
-        imageUrl: item.sunglass.imageUrl,
-        quantity: item.quantity,
-        price: item.sunglass.price,
-        sunglassId: item.sunglass.id,
-        userId: this.props.user.id
-      }
-    })
+    removeItem(sunglassesId) {
+        const { cartItems } = this.state
+        const updatedCartItems = cartItems.filter(item => {
+            return item.sunglass.id !== sunglassesId
+        })
+        axios.delete(`/api/cart/${sunglassesId}`, {data: {userId: this.props.user.id}})
+        this.setState({cartItems: updatedCartItems})
+    }
 
-    axios.post('/api/order', {orderItems: orders})
-    axios.delete('/api/cart', {data: {userId: this.props.user.id}})
-    this.setState({cartItems: []})
-  }
+    updateCart() {
+        this.state.cartItems.forEach(item => {
+            axios.put('/api/cart', {
+                userId: this.props.user.id,
+                sunglassId: item.sunglass.id,
+                quantity: item.quantity
+            })
+        })
 
-  async componentDidMount() {
-    const {data} = await axios.get('/api/cart')
-    this.setState({cartItems: data})
-  }
-  render() {
-    return (
-      <div>
-        {this.state.cartItems.length ? (
-          <div>
-            {this.state.cartItems.map(item => {
-              return (
-                <CartItem
-                  key={item.id}
-                  sunglasses={item.sunglass}
-                  quantity={item.quantity}
-                />
-              )
-            })}
-            <Button
-              variant="contained"
-              color="primary"
-              type="button"
-              onClick={this.placeOrder}
-            >
-              Place Order
-            </Button>
-          </div>
-        ) : (
-          <div>There are no items in your cart!</div>
-        )}
-      </div>
-    )
-  }
+        this.props.history.push('/checkout')
+    }
+
+    async componentDidMount() {
+        const { data } = await axios.get('/api/cart')
+        this.setState({cartItems: data})
+    }
+    render() {
+        return (
+        <div>
+            {
+             (this.state.cartItems.length) ? 
+                <div>
+                    {
+                        this.state.cartItems.map((item, index) => {
+                            return <CartItem key={item.id} 
+                                            sunglasses={item.sunglass}
+                                            quantity={item.quantity} 
+                                            removeItem={this.removeItem} 
+                                            handleChange={this.handleChange} 
+                                            index={index}/>
+                        }) 
+                    }
+
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        type="button"
+                        onClick={this.updateCart}
+                        >
+                        Check out
+                    </Button>
+                </div>
+                
+            : <div>There are no items in your cart!</div>
+            }
+        </div>
+        )
+    }
 }
 
 const mapState = state => {
--- server/api/orders.js
+++ server/api/orders.js
@@ -1,9 +1,14 @@
 const router = require('express').Router()
-const { OrderItem } = require('../db/models')
+const { OrderItem, Sunglasses } = require('../db/models')
 
 router.post('/', async (req, res, next) => {
     try {
             const newOrders = await OrderItem.bulkCreate(req.body.orderItems, {returning: true})
+            newOrders.forEach(async item => {
+                let sunglasses = await Sunglasses.findByPk(item.sunglassId)
+                let currentInventory = sunglasses.inventory
+                await sunglasses.update({inventory: currentInventory - item.quantity})
+            })
             res.json(newOrders)
         
     } catch (error) {
