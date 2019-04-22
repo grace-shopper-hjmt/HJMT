65967434a0f97c072f68c1711dd023c627749f7e
Helen Li <hca1314@gmail.com>
2019-04-22 16:08:25 -0500

    added min (1) and max (number in inventory) values for cart items

--- client/components/cart-item.js
+++ client/components/cart-item.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 export const CartItem = (props) => {
-    const {id, name, price} = props.sunglasses
+    const {id, name, price, inventory} = props.sunglasses
     return (
         <div>
             <h2>Product name: {name}</h2>
             <h3>Price: ${price / 100} each</h3>
             <label htmlFor="quantity">
-                <input name="quantity" type="number" value={props.quantity} onChange={() => props.handleChange(event, props.index)}></input>
+                <input name="quantity" type="number" value={props.quantity} min={1} max={inventory} onChange={() => props.handleChange(event, props.index)}></input>
             </label>
             <button type="button" onClick={() => props.removeItem(id)}>REMOVE ITEM</button>
         </div>
