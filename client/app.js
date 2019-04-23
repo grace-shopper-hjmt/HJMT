2f37a7d1c63af73d6312b9dd6cb8be1474d43a12
Janelle Henney <janellehenney@gmail.com>
2019-04-23 11:14:43 -0500

    trying to merge nav bar functionality to material ui stuff

--- client/app.js
+++ client/app.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import {Navbar} from './components'
+// import {Navbar} from './components'
+import {AppBar} from './components'
 import Routes from './routes'
 import CssBaseline from '@material-ui/core/CssBaseline'
 
 const App = () => {
   return (
     <div>
-      <Navbar />
+      <AppBar />
       <Routes />
       <CssBaseline />
     </div>
