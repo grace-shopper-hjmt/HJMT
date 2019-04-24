983b1e77b1e626809863b78ccd39dd6d17569505
Janelle Henney <janellehenney@gmail.com>
2019-04-24 13:25:56 -0500

    for real this time

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -5,15 +5,15 @@ import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
-  <div className='navbar'>
-    <h1 className='title'>SHADY BUSINESS</h1>
+  <div className="navbar">
+    <h1 className="title">SHADY BUSINESS</h1>
     <nav>
       {isLoggedIn ? (
         <div>
           {/* The navbar will show these links after you log in */}
           <Link to="/home">Home</Link>
-          <Link to='/users'>Admin</Link>
-          <Link to='/sunglasses'>Sunglasses</Link>
+          <Link to="/users">Admin</Link>
+          <Link to="/sunglasses">Sunglasses</Link>
           <Link to="/cart">Cart</Link>
           <a href="#" onClick={handleClick}>
             Logout
@@ -23,8 +23,8 @@ const Navbar = ({handleClick, isLoggedIn}) => (
         <div>
           {/* The navbar will show these links before you log in */}
           <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
-            <Link to='/sunglasses'>Sunglasses</Link>
+          <Link to="/signup">Sign Up</Link>
+          <Link to="/sunglasses">Sunglasses</Link>
           <Link to="/cart">Cart</Link>
         </div>
       )}
